8e9c2e777408df324b6cc0c3d6e1308c0bc04da2
Leonardo Mendes de Moraes <leonardomm4@al.insper.edu.br>
2022-06-07 17:33:37 -0300

    canvas implementados e melhorias

---
+++ Assets/_Scripts/Buttons.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+ 
+public class Buttons : MonoBehaviour
+{
+    [SerializeField] private AudioSource button_sound;
+
+    public void RestartScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        button_sound.Play();
+    }
+
+    public void QuitGame(){
+        button_sound.Play();
+        Application.Quit();
+    }
+}
